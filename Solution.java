5179b816690da987d06054750d2c10abb9cc73d6
mgruben <GrubenM@gmail.com>
2016-09-17 14:55:15 -0500

    Implement inner triangle filling

--- Solution.java
+++ Solution.java
@@ -29,6 +29,19 @@ public class Solution {
         List<List<Integer>> ans = new ArrayList<>();
         if (numRows <= 0) return ans;
         ans.add(new ArrayList<>(Arrays.asList(1)));
+        if (numRows == 1) return ans;
+        ans.add(new ArrayList<>(Arrays.asList(1, 1)));
+        int i = 2;
+        while (i < numRows) {
+            ArrayList<Integer> row = new ArrayList<>();
+            row.add(1);
+            for (int j = 1; j < i; j++) {
+                row.add(ans.get(i-1).get(j-1) + ans.get(i-1).get(j));
+            }
+            row.add(1);
+            ans.add(row);
+            i++;
+        }
         return ans;
     }
     /**
@@ -36,7 +49,7 @@ public class Solution {
      */
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.generate(1));
+        System.out.println(sol.generate(5));
     }
     
 }
