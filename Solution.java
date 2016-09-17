90041a1729c254c1729753f7389c754b90bae08f
mgruben <GrubenM@gmail.com>
2016-09-17 14:45:44 -0500

    Use diamond operator

--- Solution.java
+++ Solution.java
@@ -28,7 +28,7 @@ public class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> ans = new ArrayList<>();
         if (numRows <= 0) return ans;
-        ans.add(new ArrayList<Integer>(Arrays.asList(1)));
+        ans.add(new ArrayList<>(Arrays.asList(1)));
         return ans;
     }
     /**
